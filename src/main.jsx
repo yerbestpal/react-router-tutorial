89bac0a4e85e2e63cb145a7caae1b8d2fd2d432a
Ross McLean <ri.mcl@icloud.com>
2023-07-06 20:21:34 +0100

    Implement root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+	{
+		// This is the root route. It serves as the root layout of the UI.
+		path: '/',
+		element: <Root />,
+	},
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
