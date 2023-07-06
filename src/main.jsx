a63cb637f4b8e4be93f8b7745cb1d14f94c8916f
Ross McLean <ri.mcl@icloud.com>
2023-07-06 20:26:32 +0100

    Implement contact route

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
 	{
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/contacts/:contactId',
+		element: <Contact />,
 	},
 ])
 
