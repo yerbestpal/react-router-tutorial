6f326c389d43c1bd6fbb1b98920a5cf6ac198d62
Ross McLean <ri.mcl@icloud.com>
2023-07-07 12:51:20 +0100

    Implement edit UI

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from './error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import EditContact from './routes/edit'
 
 const router = createBrowserRouter([
 	{
@@ -21,6 +22,14 @@ const router = createBrowserRouter([
 				// The value of the parameter will be available in the route's params.
 				path: '/contacts/:contactId',
 				element: <Contact />,
+				loader: contactLoader,
+			},
+			{
+				path: '/contacts/:contactId/edit',
+				element: <EditContact />,
+				// Reusing the same loader function as the Contact route.
+				// This means the data for the Contact route will be reused for the EditContact route.
+				// This is lazy but purely for the needs of this tutorial. Ideally, you'd have a separate loader function for each route.
 				loader: contactLoader,
 			},
 		],
