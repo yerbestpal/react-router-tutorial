48cdacb4e7eafe0fa31f33f0c93b8aeff41d9bd5
Ross McLean <ri.mcl@icloud.com>
2023-07-09 19:50:15 +0100

    :memo: Add comment

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -1,6 +1,7 @@
-import { redirect } from 'react-router-dom'
-import { deleteContact } from '../contacts.js'
+import {redirect} from 'react-router-dom'
+import {deleteContact} from '../contacts.js'
 
+// Action function is called to destroy data.
 export async function action({ params }) {
   await deleteContact(params.contactId)
   return redirect('/')
