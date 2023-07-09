1cdae9885fe44f046b6f70cfdbc46b06df901f58
Ross McLean <ri.mcl@icloud.com>
2023-07-09 20:10:59 +0100

    :sparkles: Implement cancel button

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,4 @@
-import {Form, redirect, useLoaderData} from 'react-router-dom'
+import {Form, redirect, useLoaderData, useNavigate} from 'react-router-dom'
 import {updateContact} from '../contacts'
 
 // Action function is called to create data.
@@ -11,6 +11,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
   const { contact } = useLoaderData()
+  const navigate = useNavigate()
 
   return (
     <Form method="post" id="contact-form">
@@ -56,7 +57,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button" onClick={() => navigate(-1)}>
+          Cancel
+        </button>
       </p>
     </Form>
   )
