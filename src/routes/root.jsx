27dff1aff7fd1f39a049cf7b9587193863641bc0
Ross McLean <ri.mcl@icloud.com>
2023-07-15 18:06:30 +0100

    :sparkles: Implement search spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import {Form, NavLink, Outlet, redirect, useLoaderData, useNavigation,} from 'react-router-dom'
+import {Form, NavLink, Outlet, redirect, useLoaderData, useNavigation, useSubmit,} from 'react-router-dom'
 import {createContact, getContacts} from '../contacts'
 import {useEffect} from 'react'
 
@@ -22,6 +22,11 @@ export async function loader({ request }) {
 export default function Root() {
   const { contacts, q } = useLoaderData()
   const navigation = useNavigation()
+  const submit = useSubmit()
+
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q')
 
   useEffect(() => {
     document.getElementById('q').value = q
@@ -38,13 +43,19 @@ export default function Root() {
           <Form id="search-form" role="search">
             <input
               id="q"
+              className={searching ? 'loading' : ''}
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
               defaultValue={q}
+              // The `submit` hook is used to submit the form.
+              // The `submit` hook is a function that takes a form element as an argument.
+              // It returns a function that submits the form.
+              // Thus, the UI updates when the form is updated on change.
+              onChange={(event) => submit(event.currentTarget.form)}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div id="search-spinner" aria-hidden hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
           </Form>
           {/* The Form component uses client-side routing and send form data to a route action. */}
